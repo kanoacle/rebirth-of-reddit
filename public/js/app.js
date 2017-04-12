8eaef7253a07ab97ff580987d48038a53db58724
Kanojah <kanoa.hi@gmail.com>
2017-04-12 09:19:13 -1000

    doin it ya

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,17 @@ const dataReq = (source, element) => {
 
   dReq.addEventListener(`load`, function () {
     const data = JSON.parse(this.responseText);
+    console.log(data);
     const content = document.querySelector('#content');
-    for (var i = 0; i < data.data.children.length; i++) {
+    for (var i = 0; i < 4; i++) {
       const title = document.createElement('h2');
       const about = document.createElement('p');
       const blurb = document.createElement('p');
       title.innerHTML = data.data.children[i].data.title;
+      title.className = 'title';
+      about.className = 'about';
+      about.style.fontSize = '16px';
+      about.innerHTML = `by ${data.data.children[i].data.author} • `;
       content.appendChild(title);
     }
 
