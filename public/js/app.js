c6157116f7da4ed34cbb00caabcfa92c512534b5
Kanojah <kanoa.hi@gmail.com>
2017-04-12 19:33:19 -1000

    kinda sorta, not perfect however

--- public/js/app.js
+++ public/js/app.js
@@ -6,21 +6,32 @@ const dataReq = (source, element) => {
   dReq.addEventListener(`load`, function () {
     const data = JSON.parse(this.responseText);
     console.log(data);
-    const content = document.querySelector('#content');
-    for (var i = 0; i < 4; i++) {
-      const title = document.createElement('h2');
-      const about = document.createElement('p');
-      const blurb = document.createElement('p');
-      title.innerHTML = data.data.children[i].data.title;
-      title.className = 'title';
-      about.className = 'about';
-      about.style.fontSize = '16px';
-      about.innerHTML = `by ${data.data.children[i].data.author} • `;
-      content.appendChild(title);
+    const content = document.querySelector(`#content`);
+    for (var i = 0; i < data.data.children.length; i++) {
+      const contain = document.createElement(`div`);
+      const thumbnail = document.createElement(`img`);
+      const title = document.createElement(`a`);
+      const divider = document.createElement(`h3`);
+      const about = document.createElement(`p`);
+      contain.className = `container`;
+      thumbnail.src = data.data.children[i].data.thumbnail;
+      title.innerHTML = `${data.data.children[i].data.title}`;
+      title.href = `${data.data.children[i].data.url}`;
+      divider.innerHTML = `. . .`;
+      about.innerHTML = `by ${data.data.children[i].data.author} • ${data.data.children[i].data.score} Views`;
+      thumbnail.className = `pic`;
+      title.className = `title`;
+      divider.className = `divider`;
+      about.className = `about`;
+      contain.appendChild(thumbnail);
+      contain.appendChild(title);
+      contain.appendChild(divider);
+      contain.appendChild(about);
+      content.appendChild(contain);
     }
 
   });
-  dReq.open(`GET`, 'http://www.reddit.com/r/news.json');
+  dReq.open(`GET`, `https://www.reddit.com/top.json`);
   dReq.send();
 };
 dataReq();
